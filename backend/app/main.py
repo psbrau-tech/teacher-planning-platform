b74f402f668f27a1f354ba8f6f1c850bf40eb6db
Peter Brau <psbrau@gmail.com>
2026-08-08 14:38:13 -0500

    Mount governed standards administration routes

--- backend/app/main.py
+++ backend/app/main.py
@@ -40,6 +40,7 @@ from .reporting import (
     summarize_ai_cost,
 )
 from .schedule_exception_api import router as schedule_exception_router
+from .standards_admin_api import router as standards_admin_router
 from .standards_api import router as standards_router
 from .standards_catalog_api import router as standards_catalog_router
 from .teaching_assignment_api import router as teaching_assignment_router
@@ -60,6 +61,7 @@ app.include_router(live_planning_router)
 app.include_router(schedule_exception_router)
 app.include_router(standards_router)
 app.include_router(standards_catalog_router)
+app.include_router(standards_admin_router)
 app.include_router(ai_planning_router)
 app.include_router(ai_reflection_router)
 app.include_router(weekly_draft_router)
