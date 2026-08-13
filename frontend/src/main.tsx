abb71fb4a7a6c04dc9b7f3d45353ededd4e6e351
Peter Brau <psbrau@gmail.com>
2026-08-12 21:24:08 -0500

    Mount Pilot feedback experience

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { PilotFeedbackExperience } from "./PilotFeedbackExperience";
 import { TeacherPlanningShell } from "./TeacherPlanningShell";
 import "./workflow-overrides.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <TeacherPlanningShell />
+    <PilotFeedbackExperience />
   </React.StrictMode>,
 );
