5fe42dfa9b833e9de91509e363a4991ced460f18
Peter Brau <psbrau@gmail.com>
2026-08-13 09:22:40 -0500

    Mount selected-teacher admin reporting

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { AdminSelectedTeacherUsagePortal } from "./AdminSelectedTeacherUsagePortal";
 import { BaselineSurveyExperience } from "./BaselineSurveyExperience";
 import { OwnerActiveTimeBreakout } from "./OwnerActiveTimeBreakout";
 import { PilotFeedbackExperience } from "./PilotFeedbackExperience";
@@ -14,5 +15,6 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     <PilotFeedbackExperience />
     <ProductUsageObserver />
     <OwnerActiveTimeBreakout />
+    <AdminSelectedTeacherUsagePortal />
   </React.StrictMode>,
 );
