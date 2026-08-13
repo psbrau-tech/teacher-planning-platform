e4c3173d97726382d3545b0fb3f82bf75301706a
Peter Brau <psbrau@gmail.com>
2026-08-12 21:32:52 -0500

    Mount Product Owner usage observer

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { PilotFeedbackExperience } from "./PilotFeedbackExperience";
+import { ProductUsageObserver } from "./ProductUsageObserver";
 import { TeacherPlanningShell } from "./TeacherPlanningShell";
 import "./workflow-overrides.css";
 
@@ -8,5 +9,6 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <TeacherPlanningShell />
     <PilotFeedbackExperience />
+    <ProductUsageObserver />
   </React.StrictMode>,
 );
