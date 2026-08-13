d2dfbf8c560292801c4855a62ffc17df041baba9
Peter Brau <psbrau@gmail.com>
2026-08-12 21:34:26 -0500

    Mount Product Owner dashboard

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { PilotFeedbackExperience } from "./PilotFeedbackExperience";
+import { ProductOwnerDashboardExperience } from "./ProductOwnerDashboardExperience";
 import { ProductUsageObserver } from "./ProductUsageObserver";
 import { TeacherPlanningShell } from "./TeacherPlanningShell";
 import "./workflow-overrides.css";
@@ -9,6 +10,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <TeacherPlanningShell />
     <PilotFeedbackExperience />
+    <ProductOwnerDashboardExperience />
     <ProductUsageObserver />
   </React.StrictMode>,
 );
